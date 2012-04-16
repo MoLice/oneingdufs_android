531aee72b96f359a70e93ad1185b9c5ac96fde75
MoLice <sf.molice@gmail.com>
2012-04-16 22:11:13 +0800

    去除因消息系统变动而造成的文件失效
    Signed-off-by: MoLice <sf.molice@gmail.com>

--- src/com/molice/oneingdufs/androidpn/Notifier.java
+++ src/com/molice/oneingdufs/androidpn/Notifier.java
@@ -27,7 +27,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.widget.Toast;
 
 /** 
  * This class is to notify the user of messages with NotificationManager.<br/>
